c56a192dbec8c9eba7b271ac85466cca7e9d172b
ronyrtan <ron.yr.tan@hotmail.com>
2016-09-18 01:55:08 +1000

    added homework-8

---
+++ ron_tan/week_02/homework-8/js/ui.js
@@ -0,0 +1,11 @@
+// var checkBal = function () {
+//   var cBalance = $('#checking-balance').text()
+//   cBalance = '$' + checkingBalance;
+//   return cBalance;
+// }
+
+// var checkBal = function () {
+//   var sBalance = $('#savings-balance').text()
+//   sBalance = '$' + savingsBalance;
+//   return sBalance;
+// }
